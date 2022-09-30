e5f87b4a95cc6fa668b1cec661fbdc1e8d6c12c7
Miranda Vu <xmrnda@gmail.com>
2022-09-29 19:54:30 -0700

    added text and changed visuals

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i ++){
+    dogImages[i].style.borderRadius = '50px'
+for(let i = 0; i < dogImages.length; i ++)
+    dogImages[i].style.transform = 'rotateX(180deg)'}
+
+let dogNames = document.querySelectorAll('.dog-name');
+for(let i = 0; i < dogNames.length; i ++){
+    dogNames[i].style.textAlign = "left"
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue' 
+
+
+// Select some elements... 
